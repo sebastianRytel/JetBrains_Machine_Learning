71e1314d45c7cab05ab8e45444770856b27b7bdc
Sebastian <64482501+sebastianRytel@users.noreply.github.com>
2022-07-16 18:49:30 +0200

    Update Linear Regression from Scratch.py

--- Linear Regression from Scratch.py
+++ Linear Regression from Scratch.py
@@ -61,3 +61,90 @@ result = regCustom.fit(df[['x', 'w', 'z']], df['y'])
 y_pred = regCustom.predict(df[['x', 'w', 'z']])
 
 print(y_pred)
+
+============ STAGE3 ============
+
+from pprint import pprint
+
+import pandas as pd
+import numpy as np
+
+class CustomLinearRegression:
+
+    def __init__(self, *, fit_intercept):
+
+        self.fit_intercept = fit_intercept
+        self.coefficient = ...
+        self.intercept = ...
+        self.r2_score_result = ...
+        self.rmse_result = ...
+        self.matrix_w_ones = ...
+
+    def fit_w_intercept(self, X, y):
+        ones_numbers = [1 for _ in range(X.shape[0])]
+        X.insert(0, "ones", ones_numbers)
+        self.matrix_w_ones = X.copy(deep=True)
+        matrix_t = self.matrix_w_ones.T
+        eq_one = matrix_t @ X
+        eq_two = matrix_t @ y
+        final = np.linalg.inv(eq_one) @ eq_two
+        self.intercept = float(np.array(final)[0])
+        self.coefficient = final[1:]
+
+    def fit_without_intercept(self, X, y):
+        matrix_t = X.T
+        eq_one = matrix_t @ X
+        eq_two = matrix_t @ y
+        final = np.linalg.inv(eq_one) @ eq_two
+        self.coefficient = final
+
+
+    def fit(self, X, y):
+        if self.fit_intercept:
+            return self.fit_w_intercept(X, y)
+        return self.fit_without_intercept(X, y)
+
+    def predict(self, X):
+        if self.fit_intercept:
+            coefficients = np.append(self.intercept, self.coefficient)
+            return self.matrix_w_ones @ coefficients
+        return np.array(X @ self.coefficient)
+
+    def r2_score(self, y, ythat):
+
+        eq_top = sum([(yi - yithat)**2 for yi, yithat in zip(y, ythat)])
+        eq_bottom = sum([(yi - np.mean(y))**2 for yi in y])
+
+        self.r2_score_result = 1 - (eq_top / eq_bottom)
+        return self.r2_score_result
+
+    def rmse(self, y, ythat):
+        eq_top = sum([(yi - yithat) ** 2 for yi, yithat in zip(y, ythat)])
+        eq_semi = eq_top / len(y)
+        self.rmse_result = eq_semi**0.5
+        return self.rmse_result
+
+    def final_results(self):
+        return {"Intercept": self.intercept,
+                "Coefficient": self.coefficient,
+                "R2": self.r2_score_result,
+                "RMSE": self.rmse_result}
+
+data = {'x': [0.9, 0.5, 1.75, 2.0, 1.4, 1.5, 3.0, 1.1, 2.6, 1.9],
+        'w': [11, 11, 9, 8, 7, 7, 6, 5, 5, 4],
+        'y': [21.95, 27.18, 16.9, 15.37, 16.03, 18.15, 14.22, 18.72, 15.4, 14.69]}
+
+
+df = pd.DataFrame(data)
+
+regCustom = CustomLinearRegression(fit_intercept=True)
+
+result = regCustom.fit(df[['x', 'w']], df['y'])
+
+y_pred = regCustom.predict(df[['x', 'w']])
+
+r2_score = regCustom.r2_score(df['y'], y_pred)
+rmse = regCustom.rmse(df['y'], y_pred)
+
+print(regCustom.final_results())
+
